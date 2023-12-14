b44230fa78599abe08fa1b3e4dd50df27c4033ba
aLearningLad <mocmanca@gmail.com>
2023-12-14 07:10:26 +0200

    more slight changes to JS

--- gamelogic.js
+++ gamelogic.js
@@ -2,8 +2,6 @@ const grid = document.querySelector(".grid");
 const width = 8;
 const squares = [];
 
-//MAKE A BOARD
-
 const sugarColors = [
   "#FF0000",
   "#0000FF",
@@ -16,6 +14,7 @@ const sugarColors = [
 const createSquares = () => {
   for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
+    square.setAttribute("draggable", true);
     grid.appendChild(square);
     squares.push(square);
     let squareColor = Math.floor(Math.random() * sugarColors.length);
