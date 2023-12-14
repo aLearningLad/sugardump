7ba2fa367acdf2f85bc74892f5bc1b0bfb590d8f
aLearningLad <mocmanca@gmail.com>
2023-12-14 06:32:58 +0200

    latest updates to JS file

--- gamelogic.js
+++ gamelogic.js
@@ -1,4 +1,26 @@
 const grid = document.querySelector(".grid");
 const width = 8;
+const squares = [];
 
 //MAKE A BOARD
+
+const sugarColors = [
+  "#FF0000",
+  "#0000FF",
+  "#00FF00",
+  "#800080",
+  "#FFA500",
+  "#008080",
+];
+
+const createSquares = () => {
+  for (let i = 0; i < width * width; i++) {
+    const square = document.createElement("div");
+    grid.appendChild(square);
+    squares.push(square);
+    let squareColor = Math.floor(Math.random() * sugarColors.length);
+    square.style.backgroundColor = sugarColors[squareColor];
+  }
+};
+
+createSquares();
